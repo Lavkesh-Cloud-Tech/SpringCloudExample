533f12813ef73e565d205ed8cc8e8b2fc6fc1372
Lavkesh <l.rathi@elsevier.com>
2017-07-08 00:59:41 -0400

    Setting up eureka server

---
+++ eureka-server/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+IMAGE_NAME=lavkesh/eureka_registry_server
+IMAGE_VERSION=$1
+
+echo "======================== Start build eureka-server ====================="
+./gradlew clean build
+
+echo "======================== Start build image ======================="
+docker build --rm -t $IMAGE_NAME:$IMAGE_VERSION .
+echo "Image build => $IMAGE_NAME:$IMAGE_VERSION"
