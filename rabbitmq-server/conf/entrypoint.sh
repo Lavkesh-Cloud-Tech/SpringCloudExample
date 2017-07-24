a5ac58e28f7ffee8edff298336c3273afaa6698c
Lavkesh <l.rathi@elsevier.com>
2017-07-23 21:33:01 -0400

    Changes for ELK stack

--- rabbitmq-server/conf/entrypoint.sh
+++ rabbitmq-server/conf/entrypoint.sh
@@ -7,13 +7,13 @@ if [ -z "$RABBITMQ_USERNAME" ]; then
     exit 1
 fi
 
-if [ -z "$RABBITMQ_PASSWORD_HASH" ]; then
+if [ -z "$RABBITMQ_PASSWORD" ]; then
     #Fail
-    echo "No RABBITMQ_PASSWORD_HASH specified!"
+    echo "No RABBITMQ_PASSWORD specified!"
     exit 1
 fi
 
 sed -i.bak "s|RABBITMQ_USERNAME|$RABBITMQ_USERNAME|g" /etc/rabbitmq/custom_definitions.json
-sed -i.bak "s|RABBITMQ_PASSWORD_HASH|$RABBITMQ_PASSWORD_HASH|g" /etc/rabbitmq/custom_definitions.json
+sed -i.bak "s|RABBITMQ_PASSWORD|$RABBITMQ_PASSWORD|g" /etc/rabbitmq/custom_definitions.json
 
 exec "$@"
